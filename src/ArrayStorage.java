a54867c68a8cf20ca930d93dee40f2716a133e42
Rustam <gazizov_rustam@inbox.ru>
2020-07-19 13:01:19 +1000

    реализовал класс 'ArrayStorage'

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,26 +5,58 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        for (int i = 0; i < storage.length; i++) {
+            if(storage[i] != null)
+                storage[i] = null;
+            else
+                return;
+        }
     }
 
     void save(Resume r) {
+        storage[size()]=r;
     }
 
     Resume get(String uuid) {
+        int size = size();
+        for (int i = 0; i < size; i++) {
+            if(storage[i].uuid.equals(uuid))
+                return storage[i];
+        }
         return null;
     }
 
     void delete(String uuid) {
+        int size = size();
+        int numberOfDelResume = 0;
+        for (int i = 0; i < size; i++) {
+            if(storage[i].uuid.equals(uuid)){
+                numberOfDelResume = i;
+                break;
+            }
+        }
+        for (int i = numberOfDelResume; i < size; i++) {
+            storage[i] = storage[i+1];
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        int size = size();
+        Resume[] all = new Resume[size];
+        for (int i = 0; i < size; i++) {
+            all[i] = storage[i];
+        }
+        return all;
     }
 
     int size() {
-        return 0;
+        for (int i = 0; i < storage.length; i++) {
+            if(storage[i] == null)
+                return i;
+        }
+        return storage.length;
     }
 }
