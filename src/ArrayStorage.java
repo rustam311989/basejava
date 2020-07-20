398efb23ac761ce2311cb29bbe20ff836cfb978c
Rustam <gazizov_rustam@inbox.ru>
2020-07-20 21:31:38 +1000

    Добавил поле size, изменил название переменной all на allResume, где надо добавил фигуные скобки

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -3,31 +3,27 @@
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    private int size = 0;
 
     void clear() {
-        for (int i = 0; i < storage.length; i++) {
-            if (storage[i] != null)
-                storage[i] = null;
-            else
-                return;
-        }
+        size = 0;
     }
 
     void save(Resume r) {
-        storage[size()] = r;
+        storage[size] = r;
+        size++;
     }
 
     Resume get(String uuid) {
-        int size = size();
         for (int i = 0; i < size; i++) {
-            if (storage[i].uuid.equals(uuid))
+            if (storage[i].uuid.equals(uuid)) {
                 return storage[i];
+            }
         }
         return null;
     }
 
     void delete(String uuid) {
-        int size = size();
         int numberOfDelResume = 0;
         for (int i = 0; i < size; i++) {
             if (storage[i].uuid.equals(uuid)) {
@@ -38,25 +34,21 @@ public class ArrayStorage {
         for (int i = numberOfDelResume; i < size; i++) {
             storage[i] = storage[i + 1];
         }
+        size--;
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        int size = size();
-        Resume[] all = new Resume[size];
+        Resume[] allResume = new Resume[size];
         for (int i = 0; i < size; i++) {
-            all[i] = storage[i];
+            allResume[i] = storage[i];
         }
-        return all;
+        return allResume;
     }
 
     int size() {
-        for (int i = 0; i < storage.length; i++) {
-            if (storage[i] == null)
-                return i;
-        }
-        return storage.length;
+        return size;
     }
 }
